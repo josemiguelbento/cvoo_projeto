849146c0a87498e6524c002853a93726b96d6eb7
josemiguelbento <jose.miguel.bento@gmail.com>
2022-05-28 18:36:24 +0100

    ja nao sei

--- main.m
+++ main.m
@@ -9,15 +9,15 @@ g = 9.81;
 [cond_ini, max_deflec, inert, wing, deriv] = def_model();
 w0 = cond_ini.aa0*cond_ini.u0;
 
-%com a aprox das folhas
+%antonio nunes - com a aprox das folhas
 % a=[
-% deriv.xu deriv.xw -w0 -g*cos(cond_ini.tt0);
+% deriv.xu deriv.xw -cond_ini.u0*tan(cond_ini.aa0) -g*cos(cond_ini.tt0);
 % deriv.zu deriv.zw cond_ini.u0 -g*sin(cond_ini.tt0);
 % deriv.mu+deriv.mwp*deriv.zu deriv.mw+deriv.mwp*deriv.zw deriv.mq+deriv.mwp*cond_ini.u0 -deriv.mwp*g*sin(cond_ini.tt0);
 % 0 0 1 0];
 % b=[deriv.xde deriv.xdsp;deriv.zde 0;deriv.mde+deriv.mwp*deriv.zde deriv.mdsp;0 0];
 
-%considerando todas os coeficientes
+%considerando todos os coeficientes
 a=[
 deriv.xu deriv.xw -w0 -g*cos(cond_ini.tt0) 0;
 deriv.zu/(1-deriv.zwp) deriv.zw/(1-deriv.zwp) (cond_ini.u0+deriv.zq)/(1-deriv.zwp) -g*sin(cond_ini.tt0)/(1-deriv.zwp) 0;
@@ -38,7 +38,7 @@ sys = ss(a,b,c,d);
 
 [num_df,den_df]=ss2tf(a,b,c,d,2);
 
-[num_dsp,den_dsp]=ss2tf(a,b,c,d,3);
+%[num_dsp,den_dsp]=ss2tf(a,b,c,d,3);
 
 %sys = tf(num(1,:),den);
 %rlocus(sys)
@@ -76,12 +76,23 @@ end
 %(com diferentes variaveis pq o polinomio caracteristico
 %de um sistema nao depende das variaveis de estado que o definem)
 
+%aprox das folhas - da o mesmo q as formulas de estabilidade
 % a_fug=[
-% deriv.xu -g*cond_ini.tt0;
+% deriv.xu -g;
 % -deriv.zu/cond_ini.u0 0];
 
+%formulas de estabilidade
+%wn_fug_aprox = sqrt(-deriv.zu*g/cond_ini.u0);
+%xi_fug_aprox = -deriv.xu/2/wn_fug_aprox;
+
+%certa supostamente
+% a_fug=[
+% deriv.xu -g*cos(cond_ini.tt0);
+% -deriv.zu/(deriv.zq+cond_ini.u0) g*sin(cond_ini.tt0)/(deriv.zq+cond_ini.u0)];
+
+%do dores
 a_fug=[
-deriv.xu -g*cos(cond_ini.tt0);
+deriv.xu+deriv.zu*w0/(deriv.zq+cond_ini.u0) -w0*g*sin(cond_ini.tt0)/(deriv.zq+cond_ini.u0)-g*cos(cond_ini.tt0);
 -deriv.zu/(deriv.zq+cond_ini.u0) g*sin(cond_ini.tt0)/(deriv.zq+cond_ini.u0)];
 
 %b=[deriv.xde deriv.xdt;deriv.zde 0;deriv.mde+deriv.mwp*deriv.zde deriv.mdt;0 0];
@@ -90,11 +101,12 @@ deriv.xu -g*cos(cond_ini.tt0);
 damp(a_fug) %fugoide estavel????
 
 
+
 %% Período Curto Aproximado
 
 a_pc=[
-deriv.zw cond_ini.u0;
-deriv.mw+deriv.mwp*deriv.zw deriv.mq+deriv.mwp*cond_ini.u0];
+deriv.zw/(1-deriv.zwp) (cond_ini.u0+deriv.zq)/(1-deriv.zwp);
+deriv.mw+deriv.mwp*deriv.zw/(1-deriv.zwp) (deriv.mq+deriv.mwp*(cond_ini.u0+deriv.zq)/(1-deriv.zwp))];
 
 damp(a_pc)
 
